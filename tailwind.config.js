d2dd9a7c0979ef10e79a70642537bcb3bf5a8861
maggie-j-liu <maggie.j.liu@gmail.com>
2022-01-13 10:23:05 -0800

    update plugin and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,10 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  mode: "jit",
-  theme: {
-    extend: {
-      colors: {
-        orange: colors.orange,
-      },
-    },
-  },
-  // uncomment when not using jit mode
-  /*
-  variants: {
-    extend: {
-      backgroundColor: ["themes"],
-      gradientColorStops: ["themes"],
-      borderColor: ["themes"],
-      divideColor: ["themes"],
-      placeholderColor: ["themes"],
-      textColor: ["themes"],
-    },
-  },
-  */
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   plugins: [
     require("tailwind-themes")({
       themes: {
